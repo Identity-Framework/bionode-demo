f4411f4595bdd9aa97040603251299ae02e718f6
justin_zhang@brown.edu <justinzhang@justins-mbp-5.ncat.edu>
2017-07-13 13:46:16 -0400

    added extraction code

--- src/app/facecap.component.ts
+++ src/app/facecap.component.ts
@@ -71,13 +71,44 @@ export class FacecapComponent implements AfterViewInit {
       console.log(err.name, err.message);
     });
   }
-
+  greyscale(imageDat) : void {
+    let dataArray = imageDat.data;
+    for(let i = 0; i < dataArray.length; i += 4)
+    {
+      let red = dataArray[i];
+      let green = dataArray[i + 1];
+      let blue = dataArray[i + 2];
+      let alpha = dataArray[i + 3];
+        
+      let gray = (red + green + blue) / 3;
+        
+      dataArray[i] = gray;
+      dataArray[i + 1] = gray;
+      dataArray[i + 2] = gray;
+      dataArray[i + 3] = alpha; // not changing the transparency
+    }
+  }
+   
   canvasToImage(): any {
     let canvasElem = this.canvas.nativeElement;
-    let image = new Image();
-    image.src = canvasElem.toDataURL('image/png');
-    console.log('tried to capture image');
-    return image; // not sure of the type to use for func sig
+    
+    let context = canvasElem.getContext('2d');
+    let classifier = od.frontalface;
+    let detector = new od.detector(canvasElem.width, canvasElem.height, 1.2, classifier);
+    console.log(detector);
+    let faces = detector.detect(canvasElem);
+
+    context.drawImage(this.video.nativeElement,0,0, canvasElem.width, canvasElem.height);
+    console.log(faces);
+    
+    if(faces.length >= 1){
+      let faceData = context.getImageData(faces[0][0], faces[0][1], faces[0][2], faces[0][3]);
+      this.greyscale(faceData);
+      context.clearRect(0, 0, canvasElem.width, canvasElem.height);
+      context.putImageData(faceData,0,0);
+    }
+      
+        
   }
 
 }
